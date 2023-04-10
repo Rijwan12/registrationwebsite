3790ff817f90dbe5be9d41a66e61514ac50db114
Rijwan12 <rizwankhan17cs138@gmail.com>
2023-04-10 13:20:44 +0530

    registrationpage

---
+++ src/main/java/com/app/registration/swagger/config/SwaggerConfiguration.java
@@ -0,0 +1,22 @@
+package com.app.registration.swagger.config;
+
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfiguration {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+		.select()
+	    .apis(RequestHandlerSelectors.basePackage("com.app.registration.controller;"))
+		.paths(PathSelectors.any())
+		.build();
+		
+		
+	}
+
+}
